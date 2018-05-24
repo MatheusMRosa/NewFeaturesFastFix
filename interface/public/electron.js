c7c7afac70523019491cc402cd65f00eb12cb400
Matheus Moises da Rosa <matheusmoises.rosa@gmail.com>
2018-05-24 15:17:09 -0300

    Tentando colocar icons

--- interface/public/electron.js
+++ interface/public/electron.js
@@ -9,7 +9,7 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 1366, height: 768});
+    mainWindow = new BrowserWindow({width: 1366, height: 768, icon:path.join(__dirname,'.icons/mac/icon.icns')});
     mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.on('closed', () => mainWindow = null);
 }
