29685b3ebc967213a78becae9709d465eff6797a
Matheus Moises da Rosa <matheusmoises.rosa@gmail.com>
2018-06-11 15:13:03 -0300

    Adicionando testes unitários

--- interface/src/App.test.js
+++ interface/src/App.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 import ReactDOM from 'react-dom';
-import App from './App';
+import Login from './login/LoginScreen';
+import userReducer from './user/userReducer'
+import { loadingBarReducer, loadingBarMiddleware } from 'react-redux-loading-bar';
+import { backError } from './user/actionsUser'
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
 
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
+// Mock any GET request to /users
+// arguments for reply are (status, data, headers)
+mock.onGet(/[.]*\/api\/login[.]*/).reply(200, {
+
+});
+
+const store = createStore(combineReducers({
+    loadingBar: loadingBarReducer,
+    user: userReducer,
+}));
+
+it('Action Test', (done) => {
+
+    store.dispatch(backError());
+    if (store.getState().user.error === 0) {
+        done()
+    } else {
+        done("Erro ao chamar action")
+    }
+});
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+    // Teste de Interfaces (separa depois do redux)
+    const div = document.createElement('div');
+    ReactDOM.render(<Provider store={store}><Login /></Provider>, div);
+    // if(true){
+    //
+    // }
+    ReactDOM.unmountComponentAtNode(div);
 });
