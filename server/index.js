7d34f689875e4c8249ebe8a2f4602c13447bce54
Matheus Moises da Rosa <>
2018-10-24 16:45:35 -0300

    Ajuste de Deprecations

--- server/index.js
+++ server/index.js
@@ -51,7 +51,10 @@ app.use('/api/employee', auth, employee);
 app.listen(3030, '0.0.0.0', () => {
 
     console.log('He is alive');
-    mongoose.connect('mongodb://fastfix:fastfix@ds036967.mlab.com:36967/fastfix', (err) => {
+    mongoose.connect('mongodb://fastfix:fastfix@ds036967.mlab.com:36967/fastfix', {
+        autoIndex: false,
+        useNewUrlParser: true,
+    }, (err) => {
         if (err) {
             console.log("Failed: ", err)
         }
