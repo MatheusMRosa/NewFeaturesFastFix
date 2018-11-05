3db23162b648eca1f26960428c7caae6fea01bdb
Matheus Moises da Rosa <>
2018-11-05 13:52:14 -0200

    Mudando Icone

--- interface/public/electron.js
+++ interface/public/electron.js
@@ -4,11 +4,15 @@ const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
 const url = require('url');
+const nativeImage = electron.nativeImage;
+
+let newIcon = nativeImage.createFromPath(path.join(__dirname, './src', 'config', 'images', 'logoFavicon.png'));
+
 
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 1366, height: 768, icon:path.join(__dirname,'.icons/mac/icon.icns')});
+    mainWindow = new BrowserWindow({width: 1366, height: 768, icon:newIcon});
     mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.on('closed', () => mainWindow = null);
 }
